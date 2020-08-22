c1cbff39696cb2f2bd5c643ce8d77234dae9b3f3
saskiamaulansyahh <saskia.maulansyah@visionet.co.id>
2020-08-22 08:15:56 +0700

    Initial Commit

---
+++ controller/authController.js
@@ -0,0 +1,14 @@
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const db = require('../models');
+
+const indexLogin = (req, res) => {
+  res.render('login/login');
+
+};
+
+
+module.exports = {
+ 
+  indexLogin
+};
